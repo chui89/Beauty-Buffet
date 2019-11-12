b482b42f522edaaea760627066151ea8be5963b8
Viacheslav Churkin <beck2332@gmail.com>
2019-11-13 00:45:25 +0300

    Сверстан слайдер

--- js/main.js
+++ js/main.js
@@ -4,4 +4,11 @@ $(document).ready(function() {
     $('.fa-window-close').toggleClass('fa-window-close-change');
     $('.fa-bars').toggleClass('fa-bars-change');
   });
+
+  $('.slider').slick({
+    slidesToShow: 1,
+    arrows: false,
+    dots: true,
+    dotsClass: 'slider-dots'
+  });
 });
