c218be4cd4f283938be2fa8f8c72e5b8b0ed433f
Viacheslav Churkin <beck2332@gmail.com>
2019-11-14 21:25:27 +0300

    Меню

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
   $('.header_bars').on('click', function() {
     $('.nav').toggleClass('nav_active');
     $('.fa-window-close').toggleClass('fa-window-close-change');
-    $('.popup').toggle();
+    $('.popup').fadeToggle();
     $('.fa-bars').toggleClass('fa-bars-change');
   });
 
   $('.popup').on('click', function() {
     $('.nav').toggleClass('nav_active');
     $('.fa-window-close').toggleClass('fa-window-close-change');
-    $('.popup').toggle();
+    $('.popup').fadeToggle();
     $('.fa-bars').toggleClass('fa-bars-change');
   });
 
